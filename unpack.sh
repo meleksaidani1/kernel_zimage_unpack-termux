9d24ee29c671d4653afed60f49826aa09becb4a3
meleksaidani1 <139923297+meleksaidani1@users.noreply.github.com>
2024-01-05 10:47:34 +0100

    Update unpack.sh

--- unpack.sh
+++ unpack.sh
@@ -2,7 +2,7 @@
 BOLD="$(tput bold)"
 GREEN="$(tput setaf 2)"
 RESET="$(tput sgr0)"
-echo Kernel Z-Image Unpack ${BOLD}[${GREEN}Decker${RESET}${BOLD}]${RESET}
+echo "Kernel Z-Image Unpack ${BOLD}[${GREEN}Decker${RESET}${BOLD}]${RESET}"
 
 if [ $# -eq 0 ] ; then
     echo "Usage: $0 <Linux kernel zImage>"
@@ -10,13 +10,15 @@ if [ $# -eq 0 ] ; then
 fi
 
 echo -n -e '\x1F\x8B\x08' > gzip-signature-start.tmp
-GZIP_OFFSET_START=`grep -F -b -a -o -f gzip-signature-start.tmp "$1" | cut -d: -f1`
+GZIP_OFFSET_START=$(grep -F -b -a -o -f gzip-signature-start.tmp "$1" | cut -d: -f1)
 printf 'GZip Offset Start           : 0x%08X\n' "$GZIP_OFFSET_START"
-rm gzip-signature-start.tmp
+rm -f gzip-signature-start.tmp
+
 dd if="$1" of=image.tmp bs=1 skip="$GZIP_OFFSET_START" 2>/dev/null
-GZIP_ARCHIVE_SIZE=`gzip -dc image.tmp 2>/dev/null | gzip -9 | wc -c`
-rm image.tmp
+GZIP_ARCHIVE_SIZE=$(gzip -dc image.tmp 2>/dev/null | gzip -9 | wc -c)
+rm -f image.tmp
 printf 'GZip Compressed Data Size   : %d\n' "$GZIP_ARCHIVE_SIZE"
+
 dd if="$1" of=1_kernel_header.bin bs=1 count="$GZIP_OFFSET_START" 2>/dev/null
 dd if="$1" of=2_kernel_gzip.gz bs=1 skip="$GZIP_OFFSET_START" count="$GZIP_ARCHIVE_SIZE" 2>/dev/null
 dd if="$1" of=3_kernel_footer.bin bs=1 skip=$((GZIP_OFFSET_START+GZIP_ARCHIVE_SIZE)) 2>/dev/null
